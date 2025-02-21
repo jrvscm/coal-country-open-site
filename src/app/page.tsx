c78c545a56ca849e7f61db90ab73266c374161ee
jrvscm <chrisjarvisdev@gmail.com>
2025-02-21 15:38:56 -0700

    mobile callouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,11 @@ export default function Hero() {
         className="z-0"
       />
 
-            {/* Navbar */}
-            <nav className="absolute top-0 left-0 w-full flex justify-between items-center px-0 py-4 z-20">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-50 z-10" />
+
+      {/* Navbar */}
+      <nav className="absolute top-0 left-0 w-full flex justify-between items-center px-0 py-4 z-30">
         <Link href="/">
           <Image src="https://images.ctfassets.net/j2939n6mdbyq/3nVn09ySuMJdpRghyyVqeA/8a0f6cafff701f13c11bcdcb0201f950/modified_logo.png" alt="Tournament Logo" width={150} height={75} />
         </Link>
@@ -30,31 +33,33 @@ export default function Hero() {
         <div className="md:hidden text-white">Menu</div>
       </nav>
 
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-50 z-10" />
-
       {/* Hero Content */}
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white z-20">
-        <div className="bg-customBackground/95 rounded-lg p-16 relative shadow-2xl">
-          <h2 className="absolute -rotate-[25deg] left-[3rem] top-[2.75rem] text-lg italic text-customYellow">The</h2>
-          <h1 className="text-4xl md:text-7xl font-heading drop-shadow-custom-600 tracking-tight">Coal Country Open</h1>
-          <div className="flex space-x-4 mt-2">
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white z-20 p-4 md:p-0">
+      <div className="
+        bg-customBackground/95 rounded-lg 
+        px-6 py-10 md:px-16 md:py-16 w-full md:w-auto
+        relative shadow-2xl
+        box-border
+      ">
+          <h2 className="absolute -rotate-[25deg] left-[2rem] md:left-[3rem] top-[1rem] md:top-[2.75rem] text-lg italic text-customYellow">The</h2>
+          <h1 className="text-5xl md:text-7xl font-heading drop-shadow-custom-600 tracking-tight">Coal Country Open</h1>
+          <div className="flex mt-2 flex-row items-center justify-center">
             <Button asChild variant="default" className="
-              p-6
+              p-0 md:p-6 mr-[1rem]
               border border-customPrimary w-full
               bg-customPrimary hover:bg-customPrimary/60
               uppercase
               ">
-              <Link className="m-0 text-xl font-text" href="/register">Register</Link>
+              <Link className="m-0 text-lg md:text-xl font-text" href="/register">Register</Link>
             </Button>
             <Button asChild variant="default" className="
-              p-6
+              p-0 md:p-6
               bg-transparent w-full
               text-customYellow hover:bg-customYellow/60 hover:text-white 
               border border-customYellow
               uppercase 
               ">
-              <Link className="m-0 text-xl font-text" href="/sponsors">Sponsors</Link>
+              <Link className="m-0 text-lg md:text-xl font-text" href="/sponsors">Sponsors</Link>
             </Button>
           </div>
         </div>
